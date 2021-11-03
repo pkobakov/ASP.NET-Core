e18d8117b83a759c6f19ee97789490165c412489
Petar Kobakov <pkobakov@gmail.com>
2021-11-03 18:11:00 +0200

    Demo for educational purposes

---
+++ MyFirstWeb/MyFirstWeb/Controllers/PepiController.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MyFirstWeb.ViewModels;
+    using System;
+
+    public class PepiController : Controller
+    {
+
+        public IActionResult Index(int id) 
+        {
+            var viewModel = new IndexViewModel();
+            viewModel.Id = id;
+            viewModel.Username = this.User.Identity.Name;
+            viewModel.Age = viewModel.Year - 1974;
+
+            return this.View(viewModel);
+        
+        }
+
+
+    }
+}
---
+++ MyFirstWeb/MyFirstWeb/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWeb.ViewModels
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Year { get; set; } = DateTime.Now.Year;
+        public int Age { get; set; }
+    }
+}
