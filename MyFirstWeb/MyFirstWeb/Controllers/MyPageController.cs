2b4b4a76a99f07b49343316e928ac9a1ed1b7a49
Petar Kobakov <pkobakov@gmail.com>
2021-11-09 13:54:13 +0200

    Partial View

--- MyFirstWeb/MyFirstWeb/Controllers/MyPageController.cs
+++ MyFirstWeb/MyFirstWeb/Controllers/MyPageController.cs
@@ -1,17 +1,21 @@
 ﻿namespace MyFirstWeb.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using MyFirstWeb.ViewModels;
+    using MyFirstWeb.ViewModels.MyViewModels;
     using System;
+    using System.Linq;
 
-    public class PepiController : Controller
+
+    public class MyPageController : Controller
     {
 
         public IActionResult Index(int id) 
         {
+            var usernameTokens = this.User.Identity.Name;
+            var username = usernameTokens.Split('@', StringSplitOptions.RemoveEmptyEntries).ToArray();
             var viewModel = new IndexViewModel();
             viewModel.Id = id;
-            viewModel.Username = this.User.Identity.Name;
+            viewModel.Username = username[0].ToUpper();
             viewModel.Age = viewModel.Year - 1974;
 
             return this.View(viewModel);
--- MyFirstWeb/MyFirstWeb/ViewModels/MyViewModels/IndexViewModel.cs
+++ MyFirstWeb/MyFirstWeb/ViewModels/MyViewModels/IndexViewModel.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MyFirstWeb.ViewModels
+﻿namespace MyFirstWeb.ViewModels.MyViewModels
 {
+    using System;
+
     public class IndexViewModel
     {
         public int Id { get; set; }
