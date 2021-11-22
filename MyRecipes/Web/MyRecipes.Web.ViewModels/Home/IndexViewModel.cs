da3ac6ffad5ba387d064b7884750c5d11b726ee2
Petar Kobakov <pkobakov@gmail.com>
2021-11-23 01:09:43 +0200

    Add Service with DTO

---
+++ MyRecipes/Web/MyRecipes.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipes.Web.ViewModels.Home
+{
+   public class IndexViewModel
+    {
+        public int RecipesCount { get; set; }
+
+        public int CategoriesCount { get; set; }
+
+        public int IngredientsCount { get; set; }
+
+        public int ImagesCount { get; set; }
+
+    }
+}
