ab13ab0310207f84eb8e78b9b65d1fb7b9402ab1
Petar Kobakov <pkobakov@gmail.com>
2021-12-15 15:30:13 +0200

    Add Selenium Test

---
+++ MyFirstWeb/MyFirstweb.Tests/SeleniumTests.cs
@@ -0,0 +1,30 @@
+﻿namespace MyFirstweb.Tests
+{
+    using MyFirstWeb;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Xunit;
+    public class SeleniumTests
+    {
+        [Fact]
+        public void H1ElementIsRemovedFromPrivacyPage()
+        {
+            // Get server builder from Niki Kostov's ASP.Net Core Template
+
+            var serverFactory = new SeleniumServerFactory<Startup>();
+
+            // Chrome configuring
+            var options = new ChromeOptions();
+            options.AcceptInsecureCertificates = true;
+            options.AddArguments("--headless");
+
+            var webDriver = new ChromeDriver(options);
+
+            var indexUrl = serverFactory.RootUri;
+
+            webDriver.Navigate().GoToUrl($"{indexUrl}/Home/Privacy");
+
+            Assert.Throws<NoSuchElementException>(() => webDriver.FindElement(By.TagName("h1")));
+        }
+    }
+}
---
+++ MyFirstWeb/MyFirstweb.Tests/WebTests.cs
@@ -0,0 +1,34 @@
+﻿namespace MyFirstweb.Tests
+{
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using MyFirstWeb;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Xunit;
+    public class WebTests
+    {
+        [Fact]
+        public async Task HomePageShouldContainMyFirstWeb()
+        {
+            var webApplication = new WebApplicationFactory<Startup>();
+            HttpClient client = webApplication.CreateClient();
+
+            var response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var html = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("MyFirstWeb",html);
+        }
+
+        [Fact]
+        public async Task HomePageResponseShouldContainContentTypeHeader() 
+        {
+            var webApplication = new WebApplicationFactory<Startup>();
+            HttpClient client = webApplication.CreateClient();
+
+            var response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode(); 
+            Assert.True(response.Headers.Contains("Set-Cookie"));
+        }
+    }
+}
