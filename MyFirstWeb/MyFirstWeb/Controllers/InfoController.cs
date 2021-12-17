8cbdb09c60cbf39866f548fa1dfe553b8d3b1bc1
Petar Kobakov <pkobakov@gmail.com>
2021-12-17 22:52:04 +0200

    Add Cache

--- MyFirstWeb/MyFirstWeb/Controllers/InfoController.cs
+++ MyFirstWeb/MyFirstWeb/Controllers/InfoController.cs
@@ -1,16 +1,40 @@
 ﻿namespace MyFirstWeb.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
+    using Microsoft.Extensions.Logging;
     using MyFirstWeb.Filters;
     using MyFirstWeb.ViewModels.Info;
     using System;
+    using System.Threading;
 
     //filter configuration for the current controller
     [AddHeaderActionFilterAttribute]
     public class InfoController : Controller
     {
+        private readonly ILogger logger;
+        private readonly IMemoryCache cache;
+
+        public InfoController(
+            ILogger<InfoController> logger,
+            IMemoryCache memoryCache)
+        {
+            this.logger = logger;
+            this.cache = memoryCache;
+        }
         public IActionResult Time()
-        => this.Content(DateTime.Now.ToLongTimeString());
+        {
+           this.logger.LogInformation(54321, "User asked for the time");
+
+            if (!cache.TryGetValue<DateTime>("Data", out var cacheTime))
+            {
+                cacheTime = GetData();
+                cache.Set("Data", cacheTime);
+            }
+
+            return this.Content
+                (DateTime.Now.ToLongTimeString() + " - Cache: " + cacheTime);
+        }
 
 
         public IActionResult Date()
@@ -26,6 +50,12 @@
             return this.Json(model);
             //return this.Json(userAgent);
         
+        }
+
+        private DateTime GetData() 
+        {
+            Thread.Sleep(5000);
+            return DateTime.Now;
         }
     }
 }
